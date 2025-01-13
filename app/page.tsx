25df89fdeb2b651ccb2f0701fc622a1f354fc095
leassi <kamgadevop@gmail.com>
2025-01-13 21:12:10 +0100

    style

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-       <button className ="btn btn-accent">ReaDme</button>
+       <div className="flex items-center justify-center flex-col py-10 w-full">
+            <div>
+              <div className="flex flex-col">
+                  <h1>Prenez le controle <br /> de vos finances</h1>
+              </div>
+           </div>  
+       </div>
     </div>
   );
 }
+
