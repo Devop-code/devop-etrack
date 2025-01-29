ad571d2f740d3782249ba13b0a0fdae5bdd04b4e
Devop-code <kamgadevop@gmail.com>
2025-01-29 20:45:00 +0100

    correction des liens de navigation, mise à jour des imports de types, et amélioration de l'affichage dans la barre de navigation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,10 +18,11 @@ const Navbar = () => {
            <>
            <div className="flex justify-between items-center">
               <div className="flex text-2xl items-center font-bold">
-                Devop's<span className="text-accent">Etrack</span>
+                E<span className="text-accent">Etrack</span>
               </div>
 
               <div className="md:flex hidden">
+              <Link href={"/"} className="btn">Home</Link>
                 <Link href={"/budgets"} className="btn">Mes Budgets</Link>
                 <Link href={"/dashboard"} className="btn">Tableau de Bord</Link>  
                 <Link href={"/transactions"} className="btn">Mes Transactions</Link>
@@ -29,6 +30,7 @@ const Navbar = () => {
                </div>
            </div>
            <div className="md:hidden flex mt-2 justify-center">
+           <Link href={"/"} className="btn">Home</Link>
                 <Link href={"/budgets"} className="btn btn-sm">Mes Budgets</Link>
                 <Link href={"/dashboard"} className="btn mx-4 btn-sm">Tableau de Bord</Link>  
                 <Link href={"/transactions"} className="btn btn-sm">Mes Transactions</Link>
@@ -38,11 +40,11 @@ const Navbar = () => {
           
             <div className="flex justify-between items-center">
               <div className="flex text-2xl items-center font-bold">
-                Devop's<span className="text-accent">Etrack</span>
+                E<span className="text-accent">Etrack</span>
               </div>
                <div className=" flex mt-2 justify-center">
                 <Link href={"/sign-in"} className="btn btn-sm">Se Connecter</Link>
-                <Link href={"/sign-up"} className="btn mx-4 btn-sm btn-accent">S'inscrire</Link>
+                <Link href={"/sign-up"} className="btn mx-4 btn-sm btn-accent">S inscrire</Link>
                   
                 </div>
             </div>
